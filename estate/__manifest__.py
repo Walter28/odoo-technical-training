0f223adff60c033fb86309be39ad33edab3d577b
Walter28 <walterchristian28@gmail.com>
2024-10-15 16:19:55 +0200

    first commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name" : "Real Estate",
+    "version": "1.0",
+    "autor": "Walt28",
+    "description": """
+        Real estate module to show available properties
+    """,
+    "category": "",
+    "data": [],
+    "depends" : ["base"],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
