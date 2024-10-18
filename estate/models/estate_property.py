0b1835c59590583506779eea17abfc2bf09b644e
Walter28 <walterchristian28@gmail.com>
2024-10-18 11:17:31 +0200

    add advanced search feature within our search bar

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -15,7 +15,7 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "This model is for your real estate company"
     
-    name = fields.Char(string="Title", required=True, default="House x")
+    name = fields.Char(string="Name", required=True, default="House x")
     # this info is useless to get displayed in form view
     active = fields.Boolean(default=True, invisible=True)
     state = fields.Selection(
